e8045f5b872e093b70cafb62fcb64439b3776b5a
unknown <lcapelotto@gmail.com>
2022-05-17 16:20:11 -0300

    1.0

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,56 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.css';//
+import { Grid } from "@material-ui/core"
+
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+import Login from './paginas/login/Login';
+import CadastroUsuario from './paginas/cadastroUsuario/CadastroUsuario';
+
+import {Provider} from 'react-redux'
+import store from './store/Store' 
+import {ToastContainer} from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
+import Home from './paginas/home/home';
+import NavBar from './componentes/estaticos/navBar/NavBar';
+import CadastroTema from './componentes/temas/cadastroTema/CadastroTema';
+import ListaTema from './componentes/temas/listaTema/ListaTema';
+import ListaPostagem from './componentes/postagens/listaPostagem/ListaPostagem';
+import DeletoTema from './componentes/temas/deletaTema/DeletoTema';
+import CadastroPostagem from './componentes/postagens/cadastroPostagem/CadastroPostagem';
+import DeletoPostagem from './componentes/postagens/deletaPostagem/DeletoPostagem';
+import Footer from './componentes/estaticos/footer/Footer';
+
+
 
 function App() {
   return (
-    <div className="App">
-     <h1>Hello World</h1>
-    </div>
-  );
+  <Provider store={store}>
+    <ToastContainer/>
+    <Router>
+      <NavBar/>
+      <div style={{ minHeight: '100vh' }}>
+        <Routes>
+
+          <Route path="/" element={<Login/>}/>
+          <Route path="/home" element={<Home/>}/> 
+          <Route path="/login" element={<Login/>}/>
+          <Route path="/cadastrar" element={<CadastroUsuario/>}/>
+          <Route path="/temas" element={<ListaTema/>}/>
+          <Route path="/postagens" element={<ListaPostagem/>}/>
+          <Route path="/formularioTema" element={<CadastroTema/>}/>
+          <Route path="/formularioTema/:id" element={<CadastroTema/>}/>
+          <Route path="/deletoTema/:id" element={<DeletoTema/>}/>
+          <Route path="/formularioPostagem" element={<CadastroPostagem/>}/>
+          <Route path="/formularioPostagem/:id"element={<CadastroPostagem/>}/>
+          <Route path="/deletoPostagem/:id" element={<DeletoPostagem/>}/>
+
+        </Routes>
+      </div>
+      <Footer />
+    </Router>
+ </Provider>
+  )
 }
 
 export default App;
